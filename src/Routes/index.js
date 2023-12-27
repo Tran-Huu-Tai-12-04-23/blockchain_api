3f404560ceea32447fb1de5aa8156b1f8904717b
huutai <huutt201@gmail.com>
2023-12-27 11:41:24 +0700

    add user id

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 const productRoutes = require('./productRoutes');
 const qrRoutes = require('./qrRoutes');
+const userRoutes = require('./userRoutes');
 
 // Sử dụng các routes
 router.use('/v1/product', productRoutes);
+router.use('/v1/user', userRoutes);
 router.use('/v1/qr', qrRoutes);
 
 module.exports = router;
